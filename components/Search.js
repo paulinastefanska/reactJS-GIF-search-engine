4c8a68f792b2cf981537176943c82477a7e382ab
Paulina Stefanska <web.paulinastefanska@gmail.com>
2019-07-17 11:34:48 +0200

    Add search.js

--- components/Search.js
+++ components/Search.js
@@ -1,4 +1,15 @@
 Search = React.createClass({
+    getInitialState() {
+        return {
+            searchingText: ''
+        };
+    },
+    handleChange: function(event) {
+        var searchingText = event.target.value;
+        this.setState({
+            searchingText: searchingText
+        });
+    },
     render: function() {
         var styles = {
             fontSize: '1.5em',
