18424e98da54592328c97c2db56f5996acd2c03c
Paulina Stefanska <web.paulinastefanska@gmail.com>
2019-07-17 12:04:06 +0200

    Add the rest components

--- components/Search.js
+++ components/Search.js
@@ -6,23 +6,29 @@ Search = React.createClass({
     },
     handleChange: function(event) {
         var searchingText = event.target.value;
-        this.setState({
-            searchingText: searchingText
-        });
-    },
-    render: function() {
-        var styles = {
-            fontSize: '1.5em',
-            width: '90%',
-            maxWidth: '350px'
-        };
+        this.setState({searchingText: searchingText});
 
-    return <input
-             type="text"
-             onChange={this.handleChange}
-             placeholder="Tutaj wpisz wyszukiwaną frazę"
-             style={styles}
-             value={this.state.searchTerm}
-            />
-  }
+        if (searchingText.length > 2) {
+          this.props.onSearch(searchingText);
+        }
+    },
+
+    handleKeyUp: function(event) {
+        if (event.keyCode === 13) {
+          this.props.onSearch(this.state.searchingText);
+        }
+    },
+
+    render: function() {
+        var styles = {fontSize: '1.5em', width: '90%', maxWidth: '350px'};
+
+        return <input
+                 type="text"
+                 onChange={this.handleChange}
+                 onKeyUp={this.handleKeyUp}
+                 placeholder="Tutaj wpisz wyszukiwaną frazę"
+                 style={styles}
+                 value={this.state.searchTerm}
+                />
+    }
 });
